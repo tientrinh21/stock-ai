4604a16b40a1a097403feac5528238e8c479fdb6
tientrinh21 <tqtien.a11619@gmail.com>
2024-09-20 19:29:37 +0900

    feat (ai): process raw data before save to csv

--- ai/data/download_data.py
+++ ai/data/download_data.py
@@ -7,5 +7,7 @@ sp500 = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 tickers = sp500.Symbol.to_list()
 
 for ticker in tickers:
-    data = yf.download(ticker, period="max")
+    data = yf.download(ticker, period="5d")
+    data.reset_index(inplace=True)
+    data.rename(columns={"Date": "Datetime"}, inplace=True)
     data.to_csv(f"{ticker}.csv")
