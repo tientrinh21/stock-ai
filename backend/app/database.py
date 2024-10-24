1fb86cd74331994b601c02309cd089f16fa19c3b
tientrinh21 <tqtien.a11619@gmail.com>
2024-10-25 03:47:48 +0900

    fix (backend): resolved SSL connection close unexpectedly

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 dotenv.load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
